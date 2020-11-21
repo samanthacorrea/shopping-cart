09379c421626a9421c70bce2c11e58d479a370b5
Samantha Correa <samanthacorrealeite@gmail.com>
2020-11-21 17:08:59 -0400

    add appbar

--- frontend/shopping-cart/src/app.js
+++ frontend/shopping-cart/src/app.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Provider } from "react-redux"
 import { Store } from './redux/store'
 import PageSwitcher from './pages'
+import AppBar from './pages/common/appbar'
+
 
 const App = (props) => {
 	return (
 		<Provider store={Store}>
+			<AppBar />
             <PageSwitcher />
 		</Provider>
 	);
